ea8d3237b5ad943638a5e1898fea4146bcaed482
Mehdipour9721162221 <mehdipour.ali.9721162221@gmail.com>
2021-04-12 19:20:35 +0430

    Mehdipour

---
+++ 2.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+using namespace std;
+int main()
+{
+	int n, i, j;
+
+
+	cin >> n;
+	for (i = 0; i < n-1 ; ++i)
+	{
+		for (j = 0; j < n - 1 - i; ++j)
+			cout << " ";
+		for (j = 0; j < 2 * i + 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			cout << " ";
+
+		for (j = 0; j < 2 * (n - i) - 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+
+	return 0;
+}
