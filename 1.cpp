451fce1a0d4129582e882701b514581fb6ebe037
Mehdipour9721162221 <80389139+Mehdipour9721162221@users.noreply.github.com>
2021-04-12 19:05:54 +0430

    Square Maker

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,30 @@
-#include<iostream>
-int main(){
-	int a = 0;
-	int& b = a;
-	int c = a;
-	std::cout << a << "\t" << c << "\t" << std::endl;
-	b = 3;
-	std::cout << a << "\t" << b << c << "\t" << std::endl;
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, i, j;
+
+
+	cin >> n;
+	for (i = 0; i < n-1 ; ++i)
+	{
+		for (j = 0; j < n - 1 - i; ++j)
+			cout << " ";
+		for (j = 0; j < 2 * i + 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			cout << " ";
+
+		for (j = 0; j < 2 * (n - i) - 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+
 	return 0;
 }
